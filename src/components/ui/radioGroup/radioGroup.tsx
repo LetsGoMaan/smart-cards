466679f53915a54ed65316a2413dee89662b2760
LetsGoMan <ilikeseik@gmail.com>
2023-08-14 22:32:22 +0300

    refactor: add style and functionality to radioGroup

--- src/components/ui/radioGroup/radioGroup.tsx
+++ src/components/ui/radioGroup/radioGroup.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
 import s from './radioGroup.module.scss'
@@ -5,20 +7,31 @@ import s from './radioGroup.module.scss'
 type RadioProps = {
   disabled?: boolean
   labelText?: string
+  value: string
+  onValueChange?: (value: string) => void
 }
 
-export const Radio = ({ disabled, labelText }: RadioProps) => {
+export const Radio = ({ disabled, labelText, value, onValueChange }: RadioProps) => {
   const disableIndicator = disabled ? s.RadioGroupIndicatorDisabled : s.RadioGroupIndicator
+
+  const [radioValue, setRadioValue] = useState(value)
+
+  const radioHandler = (value: string) => {
+    setRadioValue(value)
+    onValueChange && onValueChange(value)
+  }
 
   return (
     <form>
       <RadioGroup.Root
-        className={s.RadioGroupRoot}
+        className={s.radioGroupRoot}
         defaultValue="default"
         aria-label="View density"
+        value={radioValue}
+        onValueChange={radioHandler}
       >
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <RadioGroup.Item disabled={disabled} className={s.RadioGroupItem} value="default" id="r1">
+          <RadioGroup.Item disabled={disabled} className={s.radioGroupItem} value="default" id="r1">
             <RadioGroup.Indicator className={disableIndicator} />
           </RadioGroup.Item>
           <label className={s.Label} htmlFor="r1">
@@ -26,18 +39,18 @@ export const Radio = ({ disabled, labelText }: RadioProps) => {
           </label>
         </div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <RadioGroup.Item className={s.RadioGroupItem} value="comfortable" id="r2">
-            <RadioGroup.Indicator className={s.RadioGroupIndicator} />
+          <RadioGroup.Item className={s.radioGroupItem} value="comfortable" id="r2">
+            <RadioGroup.Indicator className={s.radioGroupIndicator} />
           </RadioGroup.Item>
           <label className={s.Label} htmlFor="r2">
             Comfortable
           </label>
         </div>
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          <RadioGroup.Item className={s.RadioGroupItem} value="compact" id="r3">
-            <RadioGroup.Indicator className={s.RadioGroupIndicator} />
+          <RadioGroup.Item className={s.radioGroupItem} value="compact" id="r3">
+            <RadioGroup.Indicator className={s.radioGroupIndicator} />
           </RadioGroup.Item>
-          <label className={s.Label} htmlFor="r3">
+          <label className={s.label} htmlFor="r3">
             Compact
           </label>
         </div>
