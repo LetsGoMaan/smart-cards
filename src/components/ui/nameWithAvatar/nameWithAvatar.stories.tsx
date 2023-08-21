8ae18dcc498dbfc4126cb0f2aab5318c2fda2e7b
LetsGoMan <ilikeseik@gmail.com>
2023-08-21 20:38:14 +0300

    feat: add component NameWithAvatar with styles and stories

---
+++ src/components/ui/nameWithAvatar/nameWithAvatar.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import avatar from './../../../assets/avatar.png'
+
+import { NameWithAvatar } from '@/components/ui'
+
+const meta = {
+  title: 'Components/NameWithAvatar',
+  component: NameWithAvatar,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof NameWithAvatar>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const NameWithAvatarStory: Story = {
+  args: {
+    avatar: avatar,
+    name: 'Ivan',
+  },
+}
