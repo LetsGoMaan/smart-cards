d6ae68de2e576e6c1b8eb172d4e141bf8c4149b1
wrunika <2805nika@gmail.com>
2023-08-31 16:23:57 +0300

    refactor: make validation into login-form with zod and hookform/resolvers instead of react-hook-form validation

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,70 +1,48 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { SubmitHandler, useController, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button, CheckboxComponent, Input } from '@/components/ui'
 
-type FormValues = {
-  email: string
-  password: string
-  rememberMe: boolean
-}
+type LoginFormSchema = z.infer<typeof loginSchema>
+
+const MIN_PASSWORD_LENGTH = 3
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(MIN_PASSWORD_LENGTH),
+  rememberMe: z.boolean().default(false),
+})
+
 export const LoginForm = () => {
   const {
     register,
     handleSubmit,
     control,
     formState: { errors },
-  } = useForm<FormValues>()
+  } = useForm<LoginFormSchema>({ resolver: zodResolver(loginSchema) })
 
   const {
     field: { value, onChange },
   } = useController({ name: 'rememberMe', control, defaultValue: false })
 
-  /*const {
-    field: { value: emailValue, onChange: onEmailChange },
-  } = useController({ name: 'email', control, defaultValue: '' })
-
-  const {
-    field: { value: passwordValue, onChange: onPasswordChange },
-  } = useController({ name: 'password', control, defaultValue: '' })*/
-
-  const emailRegex =
-    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
-
-  const MIN_PASSWORD_LENGTH = 3
-
-  const onSubmit: SubmitHandler<FormValues> = data => console.log(data)
+  const onSubmit: SubmitHandler<LoginFormSchema> = data => console.log(data)
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Input
         type={'email'}
         label={'email'}
-        {...register('email', {
-          required: 'Email is required',
-          pattern: { value: emailRegex, message: 'Invalid email' },
-        })}
+        {...register('email')}
         errorMessage={errors.email?.message}
       />
-      {/*<Input type={'text'} label={'email'} value={emailValue} onChange={onEmailChange} />*/}
 
       <Input
         type={'password'}
         label={'password'}
-        {...register('password', {
-          required: 'Password is required',
-          minLength: {
-            value: MIN_PASSWORD_LENGTH,
-            message: `Password must be at least ${MIN_PASSWORD_LENGTH} characters`,
-          },
-        })}
+        {...register('password')}
         errorMessage={errors.password?.message}
       />
-      {/*<Input
-        type={'password'}
-        label={'password'}
-        value={passwordValue}
-        onChange={onPasswordChange}
-      />*/}
 
       <CheckboxComponent checked={value} onCheckedHandler={onChange} label={'remember me'} />
 
