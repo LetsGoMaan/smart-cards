3549af5db369cbb60ab82f476a20853f9efbed1e
wrunika <2805nika@gmail.com>
2023-09-22 20:55:41 +0300

    feat: add the ability to show friends empty deck in EmptyDeck

--- src/pages/empty-deck/empty-deck.tsx
+++ src/pages/empty-deck/empty-deck.tsx
@@ -4,28 +4,32 @@ import { Button, Typography } from '@/components'
 import { BackButton } from '@/pages'
 
 export const EmptyDeck = () => {
+  const myDeck = true //need to change
+
   return (
     <div className={s.emptyDeckWrapper}>
-      {/*<button className={s.backToPackListButton}>*/}
-      {/*  <img src={arrowBack} className={s.arrowLeftIcon} />*/}
-      {/*  <Typography variant={'body2'} as={'p'}>*/}
-      {/*    Back to Packs List*/}
-      {/*  </Typography>*/}
-      {/*</button>*/}
       <BackButton />
       <Typography className={s.namePack} variant={'large'}>
-        Name Pack
+        Name Pack //need to change
       </Typography>
-      <div className={s.textWrapper}>
-        <Typography className={s.textWarning} variant={'body1'} as={'p'}>
-          This pack is empty. Click add new card to fill this pack
-        </Typography>
-        <Button>
-          <Typography variant={'subtitle2'} as={'h4'}>
-            Add New Card
+      {myDeck ? (
+        <div className={s.textWrapper}>
+          <Typography className={s.textWarning} variant={'body1'} as={'p'}>
+            This pack is empty. Click add new card to fill this pack
           </Typography>
-        </Button>
-      </div>
+          <Button>
+            <Typography variant={'subtitle2'} as={'h4'}>
+              Add New Card
+            </Typography>
+          </Button>
+        </div>
+      ) : (
+        <div className={s.textWrapper}>
+          <Typography className={s.textWarning} variant={'body1'} as={'p'}>
+            This pack is empty.
+          </Typography>
+        </div>
+      )}
     </div>
   )
 }
