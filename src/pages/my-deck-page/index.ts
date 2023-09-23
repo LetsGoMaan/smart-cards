f24b8cd0b73f4b4ffc3ea4d9bbea1e9eefd291e7
wrunika <2805nika@gmail.com>
2023-09-23 09:55:42 +0300

    feat: add MyDeckPages with styles and exports

---
+++ src/pages/my-deck-page/index.ts
@@ -0,0 +1,2 @@
+export * from './my-deck-page'
+export * from './my-deck-table'
---
+++ src/pages/my-deck-page/my-deck-page.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+import { useParams } from 'react-router-dom'
+
+import s from './my-deck-page.module.scss'
+
+import { buttonForDrop, deleteOutline, editButton, playIcon } from '@/assets'
+import { Button, DropDownItem, DropDownMenu, Input, Sort, Typography } from '@/components'
+import { BackButton, EmptyDeck, MyDeckTable } from '@/pages'
+import { useGetDeckByIdQuery, useGetDeckCardsByIdQuery } from '@/services'
+
+export const MyDeckPage = () => {
+  const [sort, setSort] = useState<Sort>(null)
+  const { id } = useParams()
+  const { data, isLoading: gettingCardsLoading } = useGetDeckCardsByIdQuery({ id })
+  const { data: deckData, isLoading } = useGetDeckByIdQuery({ id })
+
+  if (data?.items.length === 0) return <EmptyDeck deckName={deckData?.name} isMyDeck={true} />
+  if (isLoading || gettingCardsLoading) return <div>loading...</div>
+
+  return (
+    <div className={s.myDeckWrapper}>
+      <BackButton />
+      <div className={s.titleAndButton}>
+        <div className={s.titleAndDrop}>
+          <Typography className={s.namePack} variant={'large'}>
+            My Pack
+          </Typography>
+          <DropDownMenu
+            align={'end'}
+            trigger={
+              <span className={s.dropButton}>
+                <img src={buttonForDrop} alt={'dropButton'} />
+              </span>
+            }
+          >
+            <DropDownItem>
+              <img src={playIcon} alt={'learn'} />
+              <Typography variant={'caption'}>Learn</Typography>
+            </DropDownItem>
+            <DropDownItem>
+              <img src={editButton} alt={'edit'} />
+              <Typography variant={'caption'}>Edit</Typography>
+            </DropDownItem>
+            <DropDownItem>
+              <img src={deleteOutline} alt={'delete'} />
+              <Typography variant={'caption'}>Delete</Typography>
+            </DropDownItem>
+          </DropDownMenu>
+        </div>
+        <Button>
+          <Typography variant={'subtitle2'} as={'h4'}>
+            Add New Card
+          </Typography>
+        </Button>
+      </div>
+      <Input type={'search'} className={s.searchInput} />
+
+      <MyDeckTable cards={data?.items} sort={sort} setSort={setSort} />
+    </div>
+  )
+}
