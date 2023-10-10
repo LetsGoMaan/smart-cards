7be83820c4aeb1414745e39ac11e559e4cc84825
wrunika <2805nika@gmail.com>
2023-10-10 20:31:18 +0300

    feat: add react-toastify into project

---
+++ src/pages/common/tostify-options/tostify-options.tsx
@@ -0,0 +1,7 @@
+import { CheckIcon } from '@radix-ui/react-icons'
+
+export const successOptions = {
+  icon: <CheckIcon style={{ background: '#8c61ff' }} />,
+  style: { background: '#333' },
+  progressStyle: { background: '#8c61ff' },
+}
