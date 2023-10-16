203a390f0aaf2ce891c34be96efeab7d82f7119e
wrunika <2805nika@gmail.com>
2023-10-16 13:37:06 +0300

    feat: add error handling for InputWithTypeFile

--- src/pages/common/input-with-type-file/input-with-type-file.tsx
+++ src/pages/common/input-with-type-file/input-with-type-file.tsx
@@ -10,12 +10,19 @@ import { Typography } from '@/components'
 type InputProps = {
   handleFileChange: (e: ChangeEvent<HTMLInputElement>) => void
   name: string
+  errorMessage?: string
   imageSrc?: string | null
   //register: UseFormRegister<FieldValues>
   //register: UseFormRegister<{ name: string; isPackPrivate: boolean; cover?: any }>
   register: any
 }
-export const InputWithTypeFile = ({ handleFileChange, register, name, imageSrc }: InputProps) => {
+export const InputWithTypeFile = ({
+  handleFileChange,
+  register,
+  name,
+  imageSrc,
+  errorMessage,
+}: InputProps) => {
   return (
     <>
       {imageSrc && <img className={s.coverPreview} src={imageSrc} alt={'image'} />}
@@ -33,6 +40,7 @@ export const InputWithTypeFile = ({ handleFileChange, register, name, imageSrc }
             onChange={handleFileChange}
           />
         </div>
+        {errorMessage ? <div className={s.errorMessage}>{errorMessage}</div> : null}
       </div>
     </>
   )
