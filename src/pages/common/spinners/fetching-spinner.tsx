a70a099d66815f13849dd2ea60051bd91c9f28d1
wrunika <2805nika@gmail.com>
2023-10-17 22:57:12 +0300

    refactor: change styles for FetchingSpinner

--- src/pages/common/spinners/fetching-spinner.tsx
+++ src/pages/common/spinners/fetching-spinner.tsx
@@ -2,33 +2,30 @@ import { CSSProperties } from 'react'
 
 import BarLoader from 'react-spinners/BarLoader'
 
-const override: CSSProperties = {
-  position: 'absolute',
-  marginLeft: '-8.562rem',
-  borderColor: '#8c61ff',
-  backgroundColor: '#bea6ff',
-  width: '100%',
-}
-
-const mainOverride: CSSProperties = {
-  position: 'absolute',
-  marginTop: '-2.062rem',
-  marginLeft: '-8.562rem',
-  borderColor: '#8c61ff',
-  backgroundColor: '#bea6ff',
-  width: '100%',
-}
-
 type SpinnerProps = {
   loading?: boolean
   isMain?: boolean
+  isProfile?: boolean
 }
-export const FetchingSpinner = ({ loading, isMain = false }: SpinnerProps) => {
+export const FetchingSpinner = ({ loading, isMain = false, isProfile = false }: SpinnerProps) => {
+  const marginTop = isMain ? '-2.062rem' : ''
+  const left = isProfile ? '0' : '-8.562rem'
+  const top = isProfile ? '3.75rem' : ''
+  const override: CSSProperties = {
+    position: 'absolute',
+    marginTop: marginTop,
+    marginLeft: left,
+    top: top,
+    borderColor: '#8c61ff',
+    backgroundColor: '#bea6ff',
+    width: '100%',
+  }
+
   return (
     <BarLoader
       color={'#8c61ff'}
       loading={loading}
-      cssOverride={isMain ? mainOverride : override}
+      cssOverride={override}
       height={4}
       speedMultiplier={1}
       aria-label={'Loading Spinner'}
